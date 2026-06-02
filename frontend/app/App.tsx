7c0bb2f4d8b735a43286b517b09122022a0d60ab
Anselme Irumva <andesanselme@gmail.com>
2026-06-02 14:07:33 +0200

    add: frontend module

---
+++ frontend/app/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to ToraChain!</h1>
+      <p>This is the frontend application.</p>
+    </div>
+  )
+}
+
+export default App
